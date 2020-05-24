842a5cdb5a0fc4347e205ae2c1e90c327177ddac
yeon <gnom878@gmail.com>
2020-05-24 22:12:18 +0900

    作業中

--- mmmmodal/jQuery.mmmmodal.js
+++ mmmmodal/jQuery.mmmmodal.js
@@ -8,85 +8,71 @@
  		duration 		: 速度を指定
  		no_scroll_back	: モーダルを表示したときに背景部分はスクロールできないようにする
  ************************************************************************************* */
-(function($) {
+/*!
+ * jQuery 4s.modal 0.0.1
+ * (c) 2020 ヨン　ジヌン <jinun.yeon@yeonce.com>
+ * MIT Licensed.
+ */
 
-	$.fn.zig_modal = function(options) {
+(function ($) {
+	$.fn.zig_modal = function (options) {
 		var defaultopt = {
-			btn_id : 'nsc_accordion_btn',
-			close_btn : false,
-			duration : 500,
-			transition : 'linear',
-			no_scroll_back : true,
-
+			close_btn: false,
+			duration: 500,
+			transition: "linear",
+			no_scroll_back: true,
 		};
 
 		options = options || {};
 		$.extend(true, defaultopt, options);
 
-		var modal = $(this);
-		var btn_id = $('#' + defaultopt.btn_id);
-		var close_btn = $('#' + defaultopt.close_btn);
+		var $modal = $(this);
 
-		btn_id.on('tap click', function(e) {
+		var btn_id = $("#" + $modal.attr("id"));
+		var close_btn = $("#" + defaultopt.close_btn);
+
+		btn_id.on("tap click", function (e) {
 			console.log("open click");
-			if (modal.is(':animated'))
-				return false
+			if ($modal.is(":animated")) return false;
 
-			//var isOpen =  (modal.css("display") == "block") ? true : false;
-				var isOpen =  isModalOpen();
-
-			if (isOpen) {
-				closeaccordion();
-			} else {
-				openaccordion();
-			}
+			isModalOpen() == true ? closeaccordion() : openaccordion();
 			e.stopPropagation();
 			return false;
-
 		});
 
 		if (close_btn) {
-			close_btn.on('tap click', function() {
-
-				if (modal.is(':animated'))
-					return false
+			close_btn.on("tap click", function () {
+				if ($modal.is(":animated")) return false;
 				closeaccordion();
 			});
 		}
 
-		$('a[href]').on('tap click', function() {
-			if (isModalOpen()==true){
+		$("a[href]").on("tap click", function () {
+			if (isModalOpen() == true) {
 				console.log("href click");
 				closeaccordion();
 			}
-
 		});
 		function isModalOpen() {
-			return (modal.css("display") == "block") ? true : false;
-
+			return $modal.css("display") == "block" ? true : false;
 		}
 		function openaccordion(duration) {
-
 			if (defaultopt.no_scroll_back) {
-				$('body').css({
-					overflow : 'hidden'
+				$("body").css({
+					overflow: "hidden",
 				});
 			}
 
-			modal.fadeIn();
-
+			$modal.fadeIn();
 		}
 
 		function closeaccordion() {
-
 			console.log("exec cloadcondition");
-			$('body').css({
-				overflow : 'auto'
+			$("body").css({
+				overflow: "auto",
 			});
 
 			modal.fadeOut(100);
-
 		}
-
 	};
 })(jQuery);
