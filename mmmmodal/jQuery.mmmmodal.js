66db1524d229f887dc2cf847c46d7db696fb16f1
yeon <gnom878@gmail.com>
2020-05-29 13:47:39 +0900

    add mmmmodal.js

--- mmmmodal/jQuery.mmmmodal.js
+++ mmmmodal/jQuery.mmmmodal.js
@@ -1,78 +1,133 @@
-/* *************************************************************************************
-	アーコディオンボタン
-  	[関連ファイル]
- 　　	js	:	plugin.zig.modal.css
- 		css	:	plugin.zig.modal.css
- 	[option説明]
- 		btn_id			: 操作するボタンIDを指定
- 		duration 		: 速度を指定
- 		no_scroll_back	: モーダルを表示したときに背景部分はスクロールできないようにする
- ************************************************************************************* */
 /*!
- * jQuery 4s.modal 0.0.1
+ * jQuery mmmmodal 0.0.1
  * (c) 2020 ヨン　ジヌン <jinun.yeon@yeonce.com>
  * MIT Licensed.
  */
 
 (function ($) {
-	$.fn.zig_modal = function (options) {
+	$.fn.mmmmodal = function (options) {
 		var defaultopt = {
-			close_btn: false,
 			duration: 500,
-			transition: "linear",
-			no_scroll_back: true,
+			background: "#000000",
+			overlay_opacity: ".5",
+			background_scroll: false,
+			close_btn: "close-btn",
+			type: "inline",
+			custom_css: {
+				position: "fixed",
+				width: "100%",
+				height: "100%",
+				top: "0",
+				left: "0",
+				transition: "background-color .6s ease-out",
+				display: "none",
+				"z-index": 1,
+			},
 		};
 
-		options = options || {};
-		$.extend(true, defaultopt, options);
+		var o = $.extend(true, defaultopt, options || {});
 
-		var $modal = $(this);
+		var main = function (idx, element) {
+			var $this = $(this),
+				$videoIframe;
 
-		var btn_id = $("#" + $modal.attr("id"));
-		var close_btn = $("#" + defaultopt.close_btn);
+			if (!/^#/g.exec($this.attr("href"))) return true;
 
-		btn_id.on("tap click", function (e) {
-			console.log("open click");
-			if ($modal.is(":animated")) return false;
+			// Make Modal Content
+			// ----------------------------------------------------------------
+			var getModalContent = function () {
+				let $ccc = $($this.attr("href"));
+				if (o.type == "inline") return $ccc;
 
-			isModalOpen() == true ? closeaccordion() : openaccordion();
-			e.stopPropagation();
-			return false;
-		});
+				var movieInfo = $ccc.find("a").fadeOut();
 
-		if (close_btn) {
-			close_btn.on("tap click", function () {
-				if ($modal.is(":animated")) return false;
-				closeaccordion();
-			});
-		}
+				if (/youtube/g.exec(movieInfo.attr("href"))) {
+					if ($ccc.find("iframe").length) return $ccc;
 
-		$("a[href]").on("tap click", function () {
-			if (isModalOpen() == true) {
-				console.log("href click");
-				closeaccordion();
-			}
-		});
-		function isModalOpen() {
-			return $modal.css("display") == "block" ? true : false;
-		}
-		function openaccordion(duration) {
-			if (defaultopt.no_scroll_back) {
-				$("body").css({
-					overflow: "hidden",
+					var $videoWrap = $("<div>").css({
+						position: "relative",
+						"padding-bottom": "56.25%",
+						"padding-top": "25px",
+						height: 0,
+					});
+
+					$videoIframe = $("<iframe>")
+						.attr({
+							src: movieInfo.attr("href").replace(/(.*)\?.*$/i, "$1") + "?enablejsapi=1&mute=0",
+							frameborder: "0",
+							allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
+							allowfullscreen: true,
+						})
+						.css({
+							position: "absolute",
+							top: 0,
+							left: "0",
+							width: "100%",
+							height: "100%",
+						});
+
+					$videoIframe.on("load", function () {});
+					return $ccc.append($videoWrap.append($videoIframe));
+				}
+			};
+
+			// Close Modal
+			// ----------------------------------------------------------------
+			var close = function (e) {
+				if (!$(e.target).closest($modalContent).length) {
+					$modalContentWrap.css("background-color", "transparent").fadeOut();
+					$("body").css({ overflow: "auto" });
+
+					if ($videoIframe) {
+						$videoIframe[0].contentWindow.postMessage(
+							'{"event":"command","func":"stopVideo","args":""}',
+							"*"
+						);
+					}
+				}
+			};
+
+			// Open Modal
+			// ----------------------------------------------------------------
+			var open = function (e) {
+				console.log("opeon");
+				$close_btn = $("<div class='" + o.close_btn + "'>");
+				$modalContentWrap.append($close_btn);
+				$modalContentWrap.show().css({
+					"background-color": hexToRgbA(o.background, o.overlay_opacity),
+					"transition-duration": o.duration / 1000 + "s",
 				});
-			}
+				if (o.background_scroll === false) {
+					$("body").css({ overflow: "hidden" });
+				}
 
-			$modal.fadeIn();
-		}
+				if ($videoIframe) {
+					$videoIframe[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
+				}
+			};
 
-		function closeaccordion() {
-			console.log("exec cloadcondition");
-			$("body").css({
-				overflow: "auto",
-			});
+			var $modalContent = getModalContent(),
+				$modalContentWrap = $modalContent.wrap($("<div>").css(o.custom_css)).parent();
 
-			modal.fadeOut(100);
-		}
+			$modalContentWrap.on("tap click", close);
+			$(document).on("tap click", "." + o.close_btn, close);
+			$this.on("tap click", open);
+		};
+		return this.each(main);
 	};
+
+	function hexToRgbA(hex, opacity) {
+		var c;
+		opacity = opacity ? opacity : 1;
+
+		if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+			c = hex.substring(1).split("");
+			if (c.length == 3) {
+				c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+			}
+			c = "0x" + c.join("");
+			return "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + "," + opacity + ")";
+		}
+		throw new Error("Bad Hex");
+	}
 })(jQuery);
